6ca25e16a28182c2d7993cb4ae9b43b8812e3ec7
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2024-05-13 11:59:42 +0100

    Add tool to remove code.

---
+++ tools/rm_ans.py
@@ -0,0 +1,27 @@
+
+# Remove pre-done code.
+
+START_PATTERN = "### START CODE HERE ###"
+END_PATTERN = "### END CODE HERE ###"
+
+def main(file):
+    new = ""
+
+    with open(file, "r") as fo:
+        active = False
+
+        for line in fo.readlines():
+            if active and END_PATTERN in line:
+                active = False
+                new += line
+            elif not active:
+                active = START_PATTERN in line
+                new += line
+
+    print(new)
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1])
+
+
